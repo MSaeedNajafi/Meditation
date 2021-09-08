2e73a3ed6483c33d93a3a15138eed36fd7f696c6
Saeed <najafisaeed@gmail.com>
2021-09-08 15:02:51 +0200

    first commit

---
+++ src/components/Colors.js
@@ -0,0 +1,61 @@
+const general = {
+  primaryColor: "#ffda6b",
+  secondaryColor: "#010606",
+  // primaryColor: "#ef9273",
+  // secondaryColor: "#010606",
+  textANDTitle: "#fff",
+};
+
+const COL = {
+  //general
+  btnPrimaryColor: "#ffda6b",
+  btnSecondaryColor: "#010606",
+  btnTextColor: "#010606",
+  topTitleColor: "#ffda6b",
+  activeColor: "#ffda6b",
+  btnSecondaryTextColor: "#fff",
+  //navigation
+  btnNavTextColor: "#010606",
+  btnHoverColor: "#fff",
+  navLinkTextColor: "#fff",
+  logoTextColor: "#fff",
+  navBackgroundColor: "#7e8846",
+  //sidebar
+  sideBarBackgroundColor: "#0d0d0d",
+  sideBarTextColor: "#fff",
+  iconColro: "#fff",
+  sideBarBtnHover: "#fff",
+  sideBarTextHoverColor: "#ffda6b",
+  //Info
+  infoContainer: "#010606",
+  infoSecondaryContainer: "#f9f9f9",
+  titleInfoPrimaryColor: "#010606",
+  tilteInfoSecondaryColor: "#f7f8fa",
+  descPrimary: "#fff",
+  descSecondary: "#010606",
+  btnTextInfoColor: "#010606",
+  btnInfoHoverColor: "#fff",
+  //hero
+  heroBackgroundColor: "#0c0c0c",
+  heroTitleColor: "#fff",
+  heroTextColor: "#fff",
+  //services
+  servicesBckColor: "#010606",
+  ServicesCardBckColor: "#fff",
+  SerivesH1Color: "#fff",
+  //footer
+  footerContainerBckColor: "#7e8846",
+  footerLinkItemsColor: "#fff",
+  FooterLinkOnHoverColor: "#ffda6b",
+  socialMediaIconColor: "#fff",
+  footerSocialMediaTextColor: "#fff",
+  //signin
+  signinIconColor: "#fff",
+  formBckGround: "#010101",
+  formH1Color: "#fff",
+  formBtnBckColor: "#ffda6b",
+  formBtnTextColor: "#fff",
+  formSpanColor: "#fff",
+};
+
+export default COL;
