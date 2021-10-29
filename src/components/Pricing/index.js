c8498d9aa8a6a84436acd26c85c6fdbc96247a6c
Saeed <najafisaeed@gmail.com>
2021-10-29 17:12:52 +0200

    pricing txt

--- src/components/Pricing/index.js
+++ src/components/Pricing/index.js
@@ -12,6 +12,7 @@ import GlobalStyles from "@mui/material/GlobalStyles";
 import Container from "@mui/material/Container";
 import { ServicesH1, ServicesP } from "../Services/ServicesElements";
 import { styled } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
 
 const CssCard = styled(Card)({
   "&:hover ": {
@@ -21,40 +22,56 @@ const CssCard = styled(Card)({
   },
 });
 
-const tiers = [
-  {
-    title: "Trial",
-    price: "0",
-    description: ["10 users included", "Help center access", "Email support"],
-    buttonText: "Sign up for free",
-    buttonVariant: "outlined",
-  },
-  {
-    title: "Group Session",
-    subheader: "Most popular",
-    price: "15",
-    description: [
-      "20 users included",
-      "Help center access",
-      "Priority email support",
-    ],
-    buttonText: "Get started",
-    buttonVariant: "contained",
-  },
-  {
-    title: "Private Session",
-    price: "30",
-    description: [
-      "50 users included",
-      "Help center access",
-      "Phone & email support",
-    ],
-    buttonText: "Contact us",
-    buttonVariant: "outlined",
-  },
-];
-
 function PricingContent() {
+  const [t, i18n] = useTranslation();
+  const tiers = [
+    {
+      title: "Group Meditation session",
+      price: "13.00",
+      description: ["90 minutes", "1 session"],
+      buttonText: "Reserveren",
+      buttonVariant: "outlined",
+    },
+    {
+      title: " Group Meditation session",
+      subheader: "Trial",
+      price: "7.50",
+      description: ["90 minutes", "1 session"],
+      buttonText: "Reserveren",
+      buttonVariant: "contained",
+    },
+    {
+      title: "Group Meditation session",
+      price: "38.00",
+      description: ["90 minutes", "3 session"],
+      buttonText: "Reserveren",
+      buttonVariant: "outlined",
+    },
+  ];
+  const tiers2 = [
+    {
+      title: "Private Meditation session",
+      price: "22.50",
+      description: ["30 minutes", "1 session", t("reiskosten")],
+      buttonText: "Reserveren",
+      buttonVariant: "outlined",
+    },
+    {
+      title: " Private Meditation session",
+      subheader: "Online",
+      price: "10.00",
+      description: ["45 minutes", "1 session", t("reiskosten")],
+      buttonText: "Reserveren",
+      buttonVariant: "contained",
+    },
+    {
+      title: "Private Meditation session",
+      price: "45.00",
+      description: ["60 minutes", "1 session", t("reiskosten")],
+      buttonText: "Reserveren",
+      buttonVariant: "outlined",
+    },
+  ];
   return (
     <>
       <div style={{ backgroundColor: "#7e8846" }}>
@@ -69,13 +86,9 @@ function PricingContent() {
           component="main"
           sx={{ pt: 8, pb: 6 }}
         >
-          <ServicesH1>Pricing</ServicesH1>
+          <ServicesH1>{t("Pricing")}</ServicesH1>
 
-          <ServicesP>
-            Quickly build an effective pricing table for your potential
-            customers with this layout. It&apos;s built with default MUI
-            components with little customization.
-          </ServicesP>
+          <ServicesP>{t("PricingTxt")}</ServicesP>
         </Container>
         {/* End hero unit */}
         <Container maxWidth="md" component="main" sx={{ pt: 8, pb: 6 }}>
@@ -171,6 +184,118 @@ function PricingContent() {
             ))}
           </Grid>
         </Container>
+      </div>
+      <div style={{ backgroundColor: "#7e8846" }}>
+        <GlobalStyles
+          styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+        />
+
+        {/* Hero unit */}
+        <Container
+          disableGutters
+          maxWidth="sm"
+          component="main"
+          sx={{ pt: 8, pb: 6 }}
+        >
+          <ServicesP>{t("privatetxt")}</ServicesP>
+        </Container>
+        {/* End hero unit */}
+        <Container maxWidth="md" component="main" sx={{ pt: 8, pb: 6 }}>
+          <Grid container spacing={5} alignItems="flex-end">
+            {tiers2.map((tier) => (
+              // Enterprise card is full width at sm breakpoint
+              <Grid
+                item
+                key={tier.title}
+                xs={12}
+                sm={tier.title === "Enterprise" ? 12 : 6}
+                md={4}
+              >
+                <CssCard>
+                  <CardHeader
+                    title={tier.title}
+                    subheader={tier.subheader}
+                    titleTypographyProps={{ align: "center" }}
+                    action={tier.title === "Pro" ? <StarIcon /> : null}
+                    subheaderTypographyProps={{
+                      align: "center",
+                    }}
+                    sx={{
+                      backgroundColor: (theme) =>
+                        theme.palette.mode === "light"
+                          ? theme.palette.grey[200]
+                          : theme.palette.grey[700],
+                    }}
+                  />
+                  <CardContent>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "baseline",
+                        mb: 2,
+                      }}
+                    >
+                      <Typography
+                        component="h2"
+                        variant="h3"
+                        color="text.primary"
+                      >
+                        €{tier.price}
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        /mo
+                      </Typography>
+                    </Box>
+                    <ul>
+                      {tier.description.map((line) => (
+                        <Typography
+                          component="li"
+                          variant="subtitle1"
+                          align="center"
+                          key={line}
+                        >
+                          {line}
+                        </Typography>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      fullWidth
+                      variant={tier.buttonVariant}
+                      // sx={{ mt: 3, mb: 2 }}
+                      style={{
+                        backgroundColor:
+                          tier.buttonVariant === "outlined"
+                            ? "white"
+                            : "#ffda6b",
+                        padding: "8px 0",
+                        color:
+                          tier.buttonVariant === "outlined"
+                            ? "#ffda6b"
+                            : "white",
+                        borderColor: "#ffda6b",
+                        // tier.buttonVariant === "outlined"
+                        //   ? "#ffda6b"
+                        //   : "white",
+                        cursor: "pointer",
+                        fontWeight: "bold",
+                        fontSize: 16,
+                        textTransform: "capitalize",
+                      }}
+                    >
+                      {tier.buttonText}
+                    </Button>
+                  </CardActions>
+                </CssCard>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        <div style={{ padding: 20 }}>
+          <ServicesP>{t("Corporate")}</ServicesP>
+        </div>
       </div>
     </>
   );
